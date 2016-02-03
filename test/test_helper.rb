dd3422d70eab8650e419fd769df1c15e75669dc0
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-02-03 12:41:26 +0000

    add ApiUser creation in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 ENV["API_ENV"] = ENV["RACK_ENV"] = ENV["RAILS_ENV"] = "test"
+TEST_API_TOKEN = "123890123890"
 
 require "minitest/autorun"
 require "rack/test"
@@ -14,6 +15,7 @@ class Minitest::Test
 
   def setup
     DatabaseCleaner.start
+    ApiUser.create(token: TEST_API_TOKEN, active: true)
   end
 
   def teardown
@@ -21,7 +23,7 @@ class Minitest::Test
   end
 
   def get_with_rabl path, params={}
-    get path, params, {"api.tilt.root" => "api"}
+    get path, {token: TEST_API_TOKEN}.merge(params), {"api.tilt.root" => "api"}
     @json_response = JSON.parse(last_response.body) rescue nil
   end
 end
