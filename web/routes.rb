8b37e554ae0fbbea7d2fa5b5e664798c577090ee
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-02-03 12:11:56 +0000

    add request routes to sinatra

--- web/routes.rb
+++ web/routes.rb
@@ -1,7 +1,29 @@
 module Web
   class Routes < Sinatra::Base
     get '/' do
-      'Hello world.'
+      erb :home, layout: :layout
+    end
+
+    get '/request' do
+      erb :request, layout: :layout
+    end
+
+    post '/request' do
+      if @new_user = create_api_user(params)
+        erb :request_success, layout: :layout
+      else
+        erb :request_error, layout: :layout
+      end
+    end
+
+    def create_api_user params
+      ApiUser.create(
+        email:      params['email'],
+        full_name:  params['fullname'],
+        company:    params['company'],
+        reason:     params['reason'],
+        active:     false
+      )
     end
   end
 end
