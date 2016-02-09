370ab52936440b1b4dd34b7da1ce72fcb11f2420
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-02-09 18:46:26 +0000

    add require 'action_view' to environment.rb
    RABL seems to need action_view to be required before
    it's initialised

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,25 @@
 require 'dotenv'
 Dotenv.load
 
+# Sinatra
 require 'rack/csrf'
 require 'sinatra'
+
+# Grape
 require 'grape'
+require 'action_view'
 require 'grape-rabl'
 require 'grape-kaminari'
-require 'active_support'
-require 'kramdown'
 
+require 'active_support'
+
+# Markdown rendering
+require 'kramdown'
 Tilt.prefer Tilt::KramdownTemplate
 
+# Configuration files
 require 'config/rabl'
 require 'config/active_record'
 
+# Models
 Dir["#{File.dirname(__FILE__)}/../models/**/*.rb"].each {|f| require f}
