4dd312db3a49f9aa16213a82f370bfa1c8ad6ad9
Stuart Watson <stuart.watson@unep-wcmc.org>
2016-02-09 11:31:39 +0000

    Changed passenger config

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,5 +27,5 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :keep_releases, 5
 
-set :passenger_restart_with_touch, false
+set :passenger_restart_with_touch, true
 
