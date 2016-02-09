ac5a3f8801b1be500b08afb2b766d555ce408138
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-02-09 13:36:05 +0000

    ApiUser#activate! refreshes token if nil

--- models/api_user.rb
+++ models/api_user.rb
@@ -2,6 +2,8 @@ class ApiUser < ActiveRecord::Base
   def activate!
     self.active = true
     save!
+
+    refresh_token unless token
   end
 
   def refresh_token
--- test/models/api_user_test.rb
+++ test/models/api_user_test.rb
@@ -9,6 +9,20 @@ class ApiUserTest < MiniTest::Test
     assert api_user.active
   end
 
+  def test_activate_sets_token_if_not_set
+    api_user = create(:api_user, active: false, token: nil)
+
+    api_user.activate!
+    refute_nil api_user.token
+  end
+
+  def test_activate_doesnt_set_token_if_set
+    api_user = create(:api_user, active: false, token: "123456")
+
+    api_user.activate!
+    assert_equal api_user.token, "123456"
+  end
+
   def test_refresh_token_sets_a_new_token_for_the_api_user
     api_user = create(:api_user, token: "a token")
 
