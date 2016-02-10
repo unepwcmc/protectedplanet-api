2704f104445e1f43b226061270e69297e58891e2
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-02-10 16:10:49 +0000

    add test for new /test endpoint

--- test/api/authentication_test.rb
+++ test/api/authentication_test.rb
@@ -5,19 +5,27 @@ class API::Authentication < MiniTest::Test
   include Rack::Test::Methods
 
   def app
-    API::V3::ProtectedAreas
+    API::Root
   end
 
-  def test_get_protected_areas_returns_401_on_wrong_token
-    get_with_rabl "/v3/protected_areas", token: "wrong token"
+  def test_get_test_returns_401_on_wrong_token
+    get_with_rabl "/test", token: "wrong token"
 
     refute last_response.ok?
     assert_equal last_response.status, 401
   end
 
-  def test_get_protected_areas_returns_401_on_inactive_user
+  def test_get_test_returns_200_on_good_token
+    user = ApiUser.create(token: "thetoken", active: true)
+    get_with_rabl "/test", {token: user.token}
+
+    assert last_response.ok?
+    assert_equal JSON.parse(last_response.body), {"status" => "Success!"}
+  end
+
+  def test_get_test_returns_401_on_inactive_user
     user = ApiUser.create(token: "thetoken", active: false)
-    get_with_rabl "/v3/protected_areas", {token: user.token}
+    get_with_rabl "/test", {token: user.token}
 
     refute last_response.ok?
     assert_equal last_response.status, 401
