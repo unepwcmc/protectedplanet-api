8005f266acce34bd534ee8b481404c88d0b8b9a3
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-02-29 18:14:30 +0000

    make new_request_notification template html

--- lib/mailer.rb
+++ lib/mailer.rb
@@ -1,10 +1,10 @@
 module Mailer
   NEW_REQUEST_TEMPLATE = ERB.new(File.read("lib/templates/new_request_notification.erb"))
-  def self.send_new_request_notification api_user
+  def self.send_new_request_notification api_user, activation_url
     Pony.mail(
       to: $secrets[:mailer][:notifications_recipients],
       subject: "New API token request",
-      body: NEW_REQUEST_TEMPLATE.result(binding)
+      html_body: NEW_REQUEST_TEMPLATE.result(binding)
     )
   end
 end
