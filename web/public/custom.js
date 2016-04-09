c42ca8c8647666a28fcddfaf6a2dca91428e1154
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-04-09 15:27:04 +0100

    change naming of expandable-sections into accordions

--- web/public/custom.js
+++ web/public/custom.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-  var expandableSections = $('.js-expandable-section');
+  var accordions = $('.js-accordion');
 
-  var enableToggling = function($sectionEl) {
-    var $switchEl = $sectionEl.find(".js-switch");
-    var $targetEl = $sectionEl.find(".js-target");
+  var enableToggling = function($accordionEl) {
+    var $switchEl = $accordionEl.find(".js-switch");
+    var $targetEl = $accordionEl.find(".js-target");
 
-    $sectionEl.find(".js-trigger").click(function(ev) {
+    $accordionEl.find(".js-trigger").click(function(ev) {
       $switchEl.toggleClass('is-open is-closed');
       $targetEl.toggleClass('u-hide');
     });
   };
 
-  expandableSections.each(function(i, sectionEl) {
-    enableToggling($(sectionEl));
+  accordions.each(function(i, accordionEl) {
+    enableToggling($(accordionEl));
   });
 });
