763f17f61d1ab83cb1ef974d8edf656f4f019177
Leonardo Wong <leonardo.wong@unep-wcmc.org>
2023-07-27 10:20:27 +0100

    try update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,16 +1,16 @@
-workers 2
-threads 1, 2
+workers 4
+threads 4, 4
 
 preload_app!
 
-port        5000
-environment "staging"
+port 5000
 
+daemonize
 app_dir = File.expand_path("../..", __FILE__)
-bind "unix://#{app_dir}/tmp/puma/puma.sock"
-stdout_redirect "#{app_dir}/logs/puma.stdout.log", "#{app_dir}/logs/puma.stderr.log", true
-pidfile "#{app_dir}/tmp/puma/pid"
-state_path "#{app_dir}/tmp/puma/state"
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+state_path "#{app_dir}tmp/pids/puma.state"
 directory "#{app_dir}/"
-activate_control_app "unix://#{app_dir}/tmp/puma/pumactl.sock"
+activate_control_app "unix://#{app_dir}/tmp/sockets/pumactl.sock"
 prune_bundler
